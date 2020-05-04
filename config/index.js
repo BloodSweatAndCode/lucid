c21b824c3a6118e7001c9c81323cb06061a843bc
BloodSweatAndCode <anthony.lukasavage@gmail.com>
2020-05-04 19:42:28 -0400

    WIP

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const config = module.exports = {
+	modDir: 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Celeste\\Mods',
 	logFile: path.join(process.cwd(), 'lucid.txt.log'),
 	tmpDir: path.join(process.cwd(), 'tmp'),
 	outputDir: path.join(process.cwd(), 'output'),
@@ -8,4 +9,5 @@ const config = module.exports = {
 };
 
 config.fixturesDir = path.join(config.testDir, 'fixtures');
-config.testMapFile = path.join(config.fixturesDir, 'test.bin');
+config.testMapName = 'test.bin';
+config.testMapFile = path.join(config.fixturesDir, config.testMapName);
