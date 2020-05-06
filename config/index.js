1e57ccd36f40ed1bbfce446e4214a4fa0542c530
Tony Lukasavage <alukasavage@innovu.com>
2020-05-06 13:26:12 -0400

    writer

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const config = module.exports = {
 	modDir: 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Celeste\\Mods',
+	// modDir: '/Users/tony/Library/Application\ Support/Steam/steamapps/common/Celeste/Celeste.app/Contents/MacOS/Mods',
 	logFile: path.join(process.cwd(), 'lucid.txt.log'),
 	tmpDir: path.join(process.cwd(), 'tmp'),
 	outputDir: path.join(process.cwd(), 'output'),
@@ -11,3 +12,4 @@ const config = module.exports = {
 config.fixturesDir = path.join(config.testDir, 'fixtures');
 config.testMapName = 'test.bin';
 config.testMapFile = path.join(config.fixturesDir, config.testMapName);
+config.testMapDstFile = path.join(config.modDir, config.testMapName);
--- lucid.js
+++ lucid.js
@@ -5,7 +5,8 @@ const Map = require('./lib/Map');
 (async function() {
 	try {
 		const map = await Map.load(config.testMapFile);
-		console.log(JSON.stringify(map, null, 2));
+		// console.log(JSON.stringify(map, null, 2));
+		map.save(config.testMapDstFile);
 	} catch (err) {
 		await log(err.stack);
 		process.exit(1);
