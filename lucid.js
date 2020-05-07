5da33e1c159eb9979fae52acf89206b5d2036bf9
Tony Lukasavage <alukasavage@innovu.com>
2020-05-07 18:05:48 -0400

    map loading

--- lucid.js
+++ lucid.js
@@ -1,12 +1,19 @@
 const config = require('./config');
+const fs = require('fs').promises;
 const log = require('./lib/log')('lucid');
-const Map = require('./lib/Map');
+const Side = require('./lib/Side');
 
 (async function() {
 	try {
-		const map = await Map.load(config.testMapFile);
+		const side = new Side();
+		await side.load(config.testMapFile);
+
+		await fs.writeFile('output/test.json', JSON.stringify(side, null, 2));
+
+		// console.log(side);
+		// const map = await Map.load(config.testMapFile);
 		// console.log(JSON.stringify(map, null, 2));
-		map.save(config.testMapDstFile);
+		// map.save(config.testMapDstFile);
 	} catch (err) {
 		await log(err.stack);
 		process.exit(1);
