84ffd0fbeaf0ceca542f632fef52c7ca6e9f32d4
Tony Lukasavage <alukasavage@innovu.com>
2020-05-11 23:30:54 -0400

    improved initialization of classes

--- lucid.js
+++ lucid.js
@@ -35,35 +35,3 @@ class Lucid {
 }
 
 module.exports = new Lucid();
-
-// (async function() {
-// 	try {
-// 		const side = new Side();
-// 		await side.load(config.testMapFile);
-// 		side.map.rooms[0].entities.push(new Entity('spinner', {
-// 			x: 152,
-// 			color: 'Blue',
-// 			attachToSolid: false,
-// 			y: 120
-// 		}));
-
-// 		console.log(side.sideName);
-
-// 		await fs.writeFile('output/test.json', JSON.stringify(side, null, 2));
-
-// 		await side.encode(config.testMapDstFile);
-
-// 		// mac sucks and won't allow you to write a stream to a package (.app dir),
-// 		// so we just copy the file from the output to the Mods dir
-// 		if (os.platform() === 'darwin') {
-// 			exec(`cp ${config.testMapDstFile} ${os.homedir() + '/Library/Application\\ Support/Steam/steamapps/common/Celeste/Celeste.app/Contents/MacOS/Mods'}`);
-// 		}
-
-// 		console.log('done');
-
-// 		// await fs.writeFile('output/test.json', JSON.stringify(side, null, 2));
-// 	} catch (err) {
-// 		console.error(err.stack);
-// 		process.exit(1);
-// 	}
-// })();
--- test/lib/class/Side.test.js
+++ test/lib/class/Side.test.js
@@ -17,36 +17,45 @@ describe('Side', () => {
 		it('instantiates', () => {
 			side.map.should.be.instanceOf(Map);
 			side.data.should.be.empty();
-			side.sideName.should.equal('');
+			side.sideName.should.equal('A');
+		});
+
+		it('instantiates with opts', () => {
+			const data = 123;
+			const sideName = 'B';
+			let side = new Side({ data, sideName });
+			side.map.should.be.instanceOf(Map);
+			side.data.should.equal(data);
+			side.sideName.should.equal(sideName);
 		});
 
 	});
 
-	describe('#load', () => {
+	describe('#decode', () => {
 
 		it('rejects when no file is provided', () => {
-			return side.load().should.be.rejectedWith('must provide a map file');
+			return side.decode().should.be.rejectedWith('must provide a map file');
 		});
 
 		it ('rejects when file is not a string', () => {
-			return side.load(123).should.be.rejectedWith('map file argument must be a string');
+			return side.decode(123).should.be.rejectedWith('map file argument must be a string');
 		});
 
 		it ('rejects when file is not a .bin file', () => {
-			return side.load('foobar.quux').should.be.rejectedWith(/must have a \.bin extension/);
+			return side.decode('foobar.quux').should.be.rejectedWith(/must have a \.bin extension/);
 		});
 
 		for (let sideName of sideNames) {
-			it(`loads ${sideName}-side with no metadata`, async () => {
+			it(`decodes ${sideName}-side with no metadata`, async () => {
 				const file = sideName === 'A' ? binFile :
 					binFile.replace(/.bin$/, `-${sideName}.bin`);
-				await side.load(file);
+				await side.decode(file);
 				side.sideName.should.equal(sideName);
 				side.map.rooms[0].name.should.equal('1');
 				side.data.should.be.empty();
 			});
 
-			it.skip(`loads ${sideName}-side with metadata`);
+			it.skip(`decodes ${sideName}-side with metadata`);
 		}
 
 	});
@@ -57,39 +66,12 @@ describe('Side', () => {
 			const file = 'Side-encode.bin';
 			const filepath = path.join(tmpDir, file);
 			const srcLenRounded = Math.round((await fs.readFile('test/fixtures/test.bin')).length / 100) * 100;
-			await side.load(binFile);
+			await side.decode(binFile);
 			await side.encode(filepath);
 			const buf = await fs.readFile(filepath);
 			(Math.round(buf.length / 100) * 100).should.equal(srcLenRounded);
 			buf.toString('utf8', 1, 12).should.equal('CELESTE_MAP');
 		});
-
-	});
-
-	describe('#loadMetadata', () => {
-
-		it('loads metadata', () => {
-			side.loadMetadata().should.eql({});
-			side.loadMetadata({}).should.eql({});
-			side.loadMetadata({ __children: [] }).should.eql({});
-			side.loadMetadata({
-				__children: [
-					{ __name: 'foo', bar: 'quux', __fake: 123 },
-					{ __name: 'nodes', bar: 'quux' },
-					{ __name: 'bsac', x: 123, __none: 432, y: 32 }
-				]
-			}).should.eql({
-				foo: { bar: 'quux' },
-				nodes: { bar: 'quux' },
-				bsac: { x: 123, y: 32 }
-			});
-		});
-
-	});
-
-	describe('#encodeMetadata', () => {
-
-		it.skip('encodes metadata');
 
 	});
 
